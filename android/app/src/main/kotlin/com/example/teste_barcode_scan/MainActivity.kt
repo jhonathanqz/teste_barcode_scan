942505846c086be6e5899926c8424ca0fdafc92c
jhonathanqz029 <74057391+jhonathanqz029@users.noreply.github.com>
2021-10-24 09:24:25 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/teste_barcode_scan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teste_barcode_scan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> startBarcodeScanStream() async {
+    try {
+      FlutterBarcodeScanner.getBarcodeStreamReceiver(
+              '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
+          .listen((barcode) => print(barcode));
+    } on PlatformException catch (ex) {
+      setState(() {
+        _scanBarcode = "Unkown error $ex";
+      });
+    } on FormatException {
+      setState(() {
+        _scanBarcode = "You pressed the back button before scanning anything";
+      });
+    } catch (e) {}
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Barcode scan')),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              alignment: Alignment.center,
+              child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                      onPressed: () => scanBarcodeNormal(),
+                      child: Text('Start barcode scan')),
+                  ElevatedButton(
+                      onPressed: () => scanQR(), child: Text('Start QR scan')),
+                  ElevatedButton(
+                    onPressed: () => startBarcodeScanStream(),
+                    child: Text(
+                      'Start barcode scan stream',
+                    ),
+                  ),
+                  Text(
+                    'Scan result : $_scanBarcode\n',
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
